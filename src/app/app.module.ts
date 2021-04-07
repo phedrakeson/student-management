2c232ddba31a4ea850e8d197ca87547fe47ce835
Gabriel Duarte <gabrield.developer@gmail.com>
2021-04-07 20:07:45 -0300

    chore: create initial setup with firebase app

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database'
+import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCksga_rsr1xqeNYvQv8hx_cezeiL1b65o",
+    authDomain: "student-managament-d9380.firebaseapp.com",
+    databaseURL: "https://student-managament-d9380-default-rtdb.firebaseio.com",
+    projectId: "student-managament-d9380",
+    storageBucket: "student-managament-d9380.appspot.com",
+    messagingSenderId: "694671887427",
+    appId: "1:694671887427:web:b64f7f24e64860b39598f6"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
