49255508504ab80a0a713b8b1c7205769506ee72
Gabriel Duarte <gabrield.developer@gmail.com>
2021-04-08 18:18:26 -0300

    feat: create routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddStudentComponent } from './student/add-student/add-student.component';
+import { EditStudentComponent } from './student/edit-student/edit-student.component';
+import { StudentListComponent } from './student/student-list/student-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/register-student', pathMatch: 'full' },
+  { path: 'register-student', component: AddStudentComponent }, 
+  { path: 'view-students', component: StudentListComponent }, 
+  { path: 'edit-student/:id', component: EditStudentComponent }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
