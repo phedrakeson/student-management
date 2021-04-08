7e0a3847159ad5324c36b96b39f5678365cd415a
Gabriel Duarte <gabrield.developer@gmail.com>
2021-04-08 19:46:52 -0300

    feat(student-list): create dataState logic and methods for component

--- src/app/student/student-list/student-list.component.ts
+++ src/app/student/student-list/student-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CrudService } from 'src/app/shared/crud.service';
+import { Student } from 'src/app/shared/student';
 
 @Component({
   selector: 'app-student-list',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentListComponent implements OnInit {
 
-  constructor() { }
+  page: number = 1;
+  Student: Student[];
+  hideWhenNoStudent: boolean = false;
+  noData: boolean = false;
+  preLoader: boolean = true;
+
+  constructor(
+    public crudService: CrudService,
+    public toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+    this.dataState();
+    let student = this.crudService.getStudentsList();
+    student.snapshotChanges().subscribe(data => {
+      this.Student = [];
+      data.forEach(item => {
+        let aux = item.payload.toJSON();
+        aux['$key'] = item.key;
+        this.Student.push(aux as Student);
+      });
+    });
+
+  }
+
+  dataState() {
+    this.crudService.getStudentsList().valueChanges().subscribe(data => {
+      this.preLoader = false;
+      if(data.length <= 0) {
+        this.hideWhenNoStudent = false;
+        this.noData = true;
+      } else {
+        this.hideWhenNoStudent = true;
+        this.noData = false;
+      }
+    });
+  }
+
+  deleteStudent(student) {
+    if(window.confirm('Are you sure you want to delete this student?')) {
+      this.crudService.deleteStudent(student.$key);
+      this.toastr.success(student.firstName + ' successfully deleted!');
+    }
   }
 
 }
